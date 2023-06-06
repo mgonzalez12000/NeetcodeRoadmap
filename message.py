5d76d4b73bbba3358ab23ee48eb9b8442e6dbdd6
Marco Gonzalez <43226912+mgonzalez12000@users.noreply.github.com>
2023-06-06 14:29:26 -0700

    Create message.py

---
+++ message.py
@@ -0,0 +1,3 @@
+# This is the main branch. All sections of the roadmap
+# will be divded on their own main branch. Using GitHub to practice
+# using git and also being able to study on whatever machine I have access to.
