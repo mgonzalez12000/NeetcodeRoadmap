8d48b537e31cbf3320721a0dd318cd76e5b0f684
mgonzalez12000 <43226912+mgonzalez12000@users.noreply.github.com>
2023-06-27 09:48:35 -0700

    Did more practice

--- BinarySeach/33SearchInRotatedSortedArray.py
+++ BinarySeach/33SearchInRotatedSortedArray.py
@@ -28,18 +28,35 @@ Output: -1
 
 
 def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
     left = 0
     right = len(nums) - 1
     while left <= right:
-        middle = (left + right) / 2
-
-        # base case
-        if nums[middle] == target:
-            return middle
-        # Left side of rotation
-        if nums[left] <= nums[middle]:
-            left = middle + 1
-        # Right side of rotation
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[left] <= nums[mid]:
+            if nums[left] <= target and target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
         else:
-            right = middle - 1
-        
+            if nums[right] >= target and target >= nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+
+
+n1, t1 = [4, 5, 6, 7, 0, 1, 2], 0
+n2, t2 = [4, 5, 6, 7, 0, 1, 2], 3
+n3, t3 = [1], 0
+
+
+print(search(n1, t1))
+print(search(n2, t2))
+print(search(n3, t3))
