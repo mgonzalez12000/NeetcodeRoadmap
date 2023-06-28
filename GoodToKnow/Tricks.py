bdcff6031c02165850a3447779b63299fe630d5a
Marco Gonzalez <43226912+mgonzalez12000@users.noreply.github.com>
2023-06-28 11:15:54 -0700

    New min and max trick note

--- GoodToKnow/Tricks.py
+++ GoodToKnow/Tricks.py
@@ -4,4 +4,7 @@ new_str = ''.join(filter(str.isalnum, example1))
 print(new_str) #WelcomeUser12
 # Lower casing all uppercase chars:
 new_str = new_str.lower()
-print(new_str) # welcomeuser12
+print(new_str) # 
+
+# finding a min and/or max for a solution
+# you can use the min() or max() function to check for a min or max value (typcailly in a loop) where both functions take in two args. Current value and new value
