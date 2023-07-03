0fe2c75eca13eabbddb316f6e8e86a38ca73009c
Marco Gonzalez <43226912+mgonzalez12000@users.noreply.github.com>
2023-07-03 12:27:10 -0700

    Update 21MergeTwoSortedLists.py

--- LinkedList/21MergeTwoSortedLists.py
+++ LinkedList/21MergeTwoSortedLists.py
@@ -19,10 +19,31 @@ Input: list1 = [], list2 = [0]
 Output: [0]
 """
 
-def mergeTwoLists(list1, list2):
-    """
-    :type list1: Optional[ListNode]
-    :type list2: Optional[ListNode]
-    :rtype: Optional[ListNode]
-    """
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def mergeTwoLists(list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        tail = dummy
+        while list1 != None and list2 != None:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1 != None:
+            tail.next = list1
+        elif list2 != None:
+            tail.next = list2
+        return dummy.next
     
